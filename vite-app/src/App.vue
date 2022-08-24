4669ead6564ca937b245c1530ea7cc3234b43900
dabinghehehe <956574869@qq.com>
2022-08-25 00:12:48 +0800

    8-25

--- vite-app/src/App.vue
+++ vite-app/src/App.vue
@@ -1,31 +1,69 @@
-<script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+    <div>
+        <div><button @click="change">改变</button></div>
+        <div>{{state}}</div>
+    </div>
+
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
+<script setup lang="ts">
+import { ar } from 'element-plus/es/locale';
+import { shallowRef, triggerRef, customRef, reactive, toRef } from 'vue';
+// let message = ref<string>("小兵")
+// let message: Ref<string> = ref("小兵")
+// let message = shallowRef({
+//     name: "小兵"
+// })
+
+// const changeMsg = () => {
+//     message.value.name = "大兵"
+//     triggerRef(message)
+// }
+
+// function MyRef<T>(value: T) {
+//     return customRef((trank, trigger) => {
+//         return {
+//             get() {
+//                 trank()
+//                 return value
+//             },
+//             set(newValue: T) {
+//                 console.log("set")
+//                 value = newValue
+//                 trigger()
+//             }
+//         }
+//     })
+// }
+// let message = MyRef<string>("xiaobing")
+// const changeMsg = () => {
+//     message.value = "dabing"
+// }
+// let message = reactive<number[]>([])
+// let obj = reactive({
+//     name: "xiaobing"
+// })
+
+// setTimeout(() => {
+//     let arr = [1, 2, 3, 4];
+//     // message = arr;
+//     message.push(...arr)
+//     console.log(message)
+// },1000)
+
+const obj = {
+    foo: 1,
+    bar: 1
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+const state = toRef(obj, 'bar')
+
+const change = () => {
+    state.value++
+
+    console.log("--->原始对象",obj)
+    console.log("--->引用对象",state)
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+</script>
+
+<style>
 </style>
---
+++ vite-app/src/views/LoginView.vue
@@ -0,0 +1,10 @@
+<template>
+    <el-button type="primary">Primary</el-button>
+
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style>
+</style>
