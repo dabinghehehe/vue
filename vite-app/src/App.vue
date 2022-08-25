08a82944e1d1d09b5a45c4ec310c8e69ffad1149
yinbing <956574869@qq.com>
2022-08-25 09:11:39 +0000

    [8-25][17:11]

--- vite-app/src/App.vue
+++ vite-app/src/App.vue
@@ -1,14 +1,40 @@
 <template>
     <div>
-        <div><button @click="change">改变</button></div>
-        <div>{{state}}</div>
+        <table border style="width:800px">
+            <thead>
+                <tr>
+                    <th>名称</th>
+                    <th>数量</th>
+                    <th>单价</th>
+                    <th>操作</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr :key="index" v-for="(item, index) in data">
+                    <td align="center">{{ item.name }}</td>
+                    <td align="center"><button>-</button>{{ item.num }}<button>+</button></td>
+                    <td align="center">{{ item.price }}</td>
+                    <td align="center">
+                        <button>
+                            删除
+                        </button>
+                    </td>
+                </tr>
+            </tbody>
+            <tfoot>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td align="center">总价:500</td>
+            </tfoot>
+        </table>
     </div>
 
 </template>
 
 <script setup lang="ts">
 import { ar } from 'element-plus/es/locale';
-import { shallowRef, triggerRef, customRef, reactive, toRef } from 'vue';
+import { shallowRef, triggerRef, customRef, reactive, toRef, computed, ref } from 'vue';
 // let message = ref<string>("小兵")
 // let message: Ref<string> = ref("小兵")
 // let message = shallowRef({
@@ -51,18 +77,55 @@ import { shallowRef, triggerRef, customRef, reactive, toRef } from 'vue';
 //     console.log(message)
 // },1000)
 
-const obj = {
-    foo: 1,
-    bar: 1
-}
-const state = toRef(obj, 'bar')
+// const obj = {
+//     foo: 1,
+//     bar: 1
+// }
+// const state = toRef(obj, 'bar')
 
-const change = () => {
-    state.value++
+// const change = () => {
+//     state.value++
 
-    console.log("--->原始对象",obj)
-    console.log("--->引用对象",state)
+//     console.log("--->原始对象",obj)
+//     console.log("--->引用对象",state)
+// }
+
+
+// let firstname = ref('')
+// let lastname = ref('')
+
+// const name = computed({
+//     get() {
+//         return firstname.value + lastname.value
+//     },
+//     set() {
+//         firstname.value + lastname.value
+//     }
+// })
+
+type Shop = {
+    name: string,
+    num: number,
+    price: number
 }
+const data = reactive<Shop[]>([
+    {
+        name: "小兵的裤子",
+        num: 1,
+        price: 120
+    },
+    {
+        name: "小兵的鞋子",
+        num: 1,
+        price: 200
+    },
+    {
+        name: "小兵的帽子",
+        num: 1,
+        price: 240
+    }
+])
+
 </script>
 
 <style>
